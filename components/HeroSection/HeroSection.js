ca33c0bb231c368ba446e1519e242c20efa8047a
Francis Hor <francishoe@gmail.com>
2023-02-04 13:23:27 +0800

    Update head stylesheet location.

--- components/HeroSection/HeroSection.js
+++ components/HeroSection/HeroSection.js
@@ -1,4 +1,3 @@
-import Head from "next/head"
 import {
     Box,
     Heading,
@@ -14,13 +13,6 @@ import {
 export const HeroSection = () => {
     return (
         <>
-            <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
-
             <Container maxW={"3xl"}>
                 <Stack as={Box} textAlign={"center"} spacing={{ base: 8, md: 14 }} py={{ base: 20, md: 36 }} >
                     <Heading fontWeight={600} fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }} lineHeight={"110%"} >
